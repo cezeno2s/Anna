8e865399527e14cf7e565fe097e154368ada9706
cezeno2s <ezenobichinedu@gmail.com>
2020-12-24 10:29:23 -0500

    it finally worked

--- anna.java
+++ anna.java
@@ -1,10 +1,12 @@
-public class
+public class anna
 {
 public static void main(String args [])
 {
-
+int x, y, z;
+x=2; y=2;
+z=x+y;
 System.out.println("hello world");
-System.out.println("The sum of 2+2: "+ 2+2 );
+System.out.println("The sum of 2+2: "+ z );
 
 }
 }
